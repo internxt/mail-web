dd382821a44ef538b8e4a781bbeb04791a311818
Xavier Abad <77491413+xabg2@users.noreply.github.com>
2026-03-04 09:59:24 +0100

    feat: welcome page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,14 +2,23 @@ import { type RouteObject, Navigate } from 'react-router-dom'
 import { lazy } from 'react'
 import RootLayout from '@/layouts/RootLayout'
 
+const WelcomePage = lazy(() => import('@/features/welcome'))
 const MailView = lazy(() => import('@/features/mail/MailView'))
 
 export const routes: RouteObject[] = [
   {
+    index: true,
+    path: '/welcome',
+    element: <WelcomePage />,
+  },
+  {
     path: '/',
     element: <RootLayout />,
     children: [
-      { index: true, element: <Navigate to="/inbox" replace /> },
+      {
+        index: true,
+        element: <Navigate to="/welcome" replace />,
+      },
       { path: 'inbox', element: <MailView folder="inbox" /> },
       { path: 'trash', element: <MailView folder="trash" /> },
       {
