f386a055790f08ab84bf94f34bdae2de60317d36
Xavier Abad <77491413+xabg2@users.noreply.github.com>
2026-03-24 09:37:46 +0100

    feat(context): dialogs manager

---
+++ src/context/dialog-manager/DialogManager.context.tsx
@@ -0,0 +1,35 @@
+import { useMemo, useState, useCallback, type ReactNode, type FC } from 'react';
+import type { ActionDialog, ActionDialogState, DialogActionConfig } from './types';
+import { DialogManagerContext } from './useActionDialog';
+
+export const DialogManagerProvider: FC<{ children: ReactNode }> = ({ children }) => {
+  const [actionDialogs, setActionDialogs] = useState<Partial<Record<ActionDialog, ActionDialogState>>>({});
+
+  const openDialog = useCallback((dialogKey: ActionDialog, config?: DialogActionConfig) => {
+    setActionDialogs((prevDialogs) => {
+      const newDialogs = config?.closeAllDialogsFirst ? {} : { ...prevDialogs };
+      newDialogs[dialogKey] = { isOpen: true, key: dialogKey, data: config?.data };
+      return newDialogs;
+    });
+  }, []);
+
+  const closeDialog = useCallback((dialogKey: ActionDialog) => {
+    setActionDialogs((prevDialogs) => {
+      return {
+        ...prevDialogs,
+        [dialogKey]: { ...prevDialogs[dialogKey], isOpen: false, data: null },
+      };
+    });
+  }, []);
+
+  const memoizedValue = useMemo(
+    () => ({
+      actionDialogs,
+      openDialog,
+      closeDialog,
+    }),
+    [actionDialogs, openDialog, closeDialog],
+  );
+
+  return <DialogManagerContext.Provider value={memoizedValue}>{children}</DialogManagerContext.Provider>;
+};
