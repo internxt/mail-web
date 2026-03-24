1b80892df2dc7b8a07fd8ddb9bd826653d9f01eb
Xavier Abad <77491413+xabg2@users.noreply.github.com>
2026-03-24 18:52:32 +0100

    fix: remove useless props

--- src/components/preferences/components/SectionItem.tsx
+++ src/components/preferences/components/SectionItem.tsx
@@ -2,21 +2,11 @@ export interface SectionItemProps {
   text: string;
   isActive?: boolean;
   isSection?: boolean;
-  isSubsection?: boolean;
   isDisabled?: boolean;
   onClick?: () => void;
-  notificationsNumber?: number;
 }
 
-const SectionItem = ({
-  text,
-  isActive,
-  isDisabled,
-  isSection,
-  isSubsection,
-  notificationsNumber,
-  onClick,
-}: SectionItemProps) => {
+const SectionItem = ({ text, isActive, isDisabled, isSection, onClick }: SectionItemProps) => {
   const isClickable = !!onClick && !isDisabled;
   const clickableContainerClass = isClickable ? 'hover:bg-gray-5' : '';
   const activeContainerClass = isActive ? 'bg-primary' : clickableContainerClass;
@@ -25,9 +15,6 @@ const SectionItem = ({
   const activeTextClass = isActive ? 'text-white' : 'text-gray-80';
   const disabledTextClass = isDisabled ? 'text-gray-40' : activeTextClass;
   const sectionTextClass = isSection ? 'font-semibold' : '';
-  const subsectionTextClass = isSubsection ? 'px-3' : '';
-  const notificationClass = isActive ? 'bg-white' : ' bg-primary';
-  const notificationTextClass = isActive ? 'text-primary' : 'text-white';
 
   const Element = isClickable ? 'button' : 'div';
 
@@ -39,15 +26,8 @@ const SectionItem = ({
       {...(isClickable ? { type: 'button' as const } : {})}
     >
       <div className="flex items-center">
-        <span className={`text-base font-normal ${disabledTextClass} ${sectionTextClass} ${subsectionTextClass}`}>
-          {text}
-        </span>
+        <span className={`text-base font-normal ${disabledTextClass} ${sectionTextClass}`}>{text}</span>
       </div>
-      {notificationsNumber && notificationsNumber > 0 && (
-        <div className={`flex h-5 min-w-5 items-center justify-center rounded-full px-1.5 ${notificationClass}`}>
-          <span className={`text-xs font-normal  ${notificationTextClass}`}>{notificationsNumber}</span>
-        </div>
-      )}
     </Element>
   );
 };
