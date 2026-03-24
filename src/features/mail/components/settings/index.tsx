834009867246722118209d4a49000290c1979ee2
Xavier Abad <77491413+xabg2@users.noreply.github.com>
2026-03-24 18:17:30 +0100

    feat: account popover

--- src/features/mail/components/settings/index.tsx
+++ src/features/mail/components/settings/index.tsx
@@ -1,13 +1,19 @@
 import { usePreferencesNavigation } from '@/hooks/preferences/usePreferencesNavigation';
 import { useAppDispatch, useAppSelector } from '@/store/hooks';
-import { logoutThunk } from '@/store/slices/user/thunks';
-import { Avatar, Button, Dropdown } from '@internxt/ui';
+import { useGetStorageLimitQuery, useGetStorageUsageQuery } from '@/store/queries/storage/storage.query';
+import { Button } from '@internxt/ui';
 import { GearIcon } from '@phosphor-icons/react';
+import AccountPopover from './components/account-popover';
+import { logoutThunk } from '@/store/slices/user/thunks';
 
 const Settings = () => {
   const dispatch = useAppDispatch();
   const user = useAppSelector((state) => state.user.user);
+  const { data: usage } = useGetStorageUsageQuery();
+  const { data: limit } = useGetStorageLimitQuery();
   const { openSection } = usePreferencesNavigation();
+
+  const percentageUsed = usage != null && limit ? Math.round((usage / limit) * 100) : 0;
 
   if (!user) return null;
 
@@ -15,29 +21,21 @@ const Settings = () => {
     openSection('general');
   };
 
-  const onLogOut = async () => {
-    await dispatch(logoutThunk());
+  const onLogout = () => {
+    dispatch(logoutThunk());
   };
-
-  const dropdownActions = [
-    {
-      name: 'Settings',
-      onClick: openPreferences,
-    },
-    {
-      name: 'Logout',
-      onClick: onLogOut,
-    },
-  ];
 
   return (
     <div className="flex flex-row w-full gap-1 items-center justify-end p-4">
       <Button variant="ghost" onClick={openPreferences}>
         <GearIcon size={24} />
       </Button>
-      <Dropdown classMenuItems="" openDirection="left" dropdownActionsContext={dropdownActions}>
-        <Avatar fullName={user.name} src={user.avatar} diameter={40} />
-      </Dropdown>
+      <AccountPopover
+        user={user}
+        percentageUsed={percentageUsed}
+        openPreferences={openPreferences}
+        onLogout={onLogout}
+      />
     </div>
   );
 };
